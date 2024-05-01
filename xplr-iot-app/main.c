665127ed48335f00acbec2c9b487cddaec256ac0
Anderson Felippe <adfelippe@gmail.com>
2024-05-01 19:13:22 +0200

    xplr-iot-app: add SPS example

--- xplr-iot-app/main.c
+++ xplr-iot-app/main.c
@@ -1,5 +1,6 @@
+
 /*
- * Copyright 2023 u-blox
+ * Copyright 2022 u-blox
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,39 +16,62 @@
  */
 
 /*
+ * Simple BLE SPS example using ubxlib
  *
- * A demo on setting up an iBeacon using ubxlib GAP/GATT apis.
- *
- * IMPORTANT! ubxlib version 1.2 or later is required.
+ * The only ubxlib BLE functionality available for now
+ * is the UBlox SPS service. This example implements a
+ * SPS echo server to which a client can connect and
+ * send data and then get that data echoed back.
+ * A typical client can be the "U-blox Bluetooth Low Energy"
+ * application available for Android and IOS.
  *
  */
 
 #include <stdio.h>
-
+#include <string.h>
 #include "ubxlib.h"
 
+static uDeviceType_t gDeviceType = U_DEVICE_TYPE_SHORT_RANGE;
+static const uNetworkCfgBle_t gNetworkCfg = {
+    .type = U_NETWORK_TYPE_BLE,
+    .role = U_BLE_CFG_ROLE_PERIPHERAL,
+    .spsServer = true
+};
+static uDeviceCfg_t gDeviceCfg;
 
-uDeviceHandle_t gDeviceHandle;
-uDeviceCfg_t gDeviceCfg;
-
-static bool scanResponseCb(uBleScanResult_t *pScanResult)
+static void connectionCallback(int32_t connHandle, char *address, int32_t status,
+                               int32_t channel, int32_t mtu, void *pParameters)
 {
-    printf("%s;%s;", pScanResult->address, pScanResult->name);
-    for (int i = 0; i < pScanResult->dataLength; i++) {
-        printf("%02X", pScanResult->data[i]);
+    if (status == (int32_t)U_BLE_SPS_CONNECTED) {
+        printf("Connected to: %s\n", address);
+    } else if (status == (int32_t)U_BLE_SPS_DISCONNECTED) {
+        if (connHandle != U_BLE_SPS_INVALID_HANDLE) {
+            printf("Diconnected\n");
+        } else {
+            printf("* Connection attempt failed\n");
+        }
     }
-    printf(";%d\n", pScanResult->rssi);
-    return true;
+}
+
+static void dataAvailableCallback(int32_t channel, void *pParameters)
+{
+    char buffer[100] = {0};
+    int32_t length;
+    uDeviceHandle_t *pDeviceHandle = (uDeviceHandle_t *)pParameters;
+    printf("dataAvailableCallback\n");
+    do {
+        length = uBleSpsReceive(*pDeviceHandle, channel, buffer, sizeof(buffer) - 1);
+        if (length > 0) {
+            buffer[length] = 0;
+            printf("Received: %s\n", buffer);
+            // Echo the received data
+            uBleSpsSend(*pDeviceHandle, channel, buffer, length);
+        }
+    } while (length > 0);
 }
 
 void main()
 {
-    static uNetworkCfgBle_t networkCfg = {
-        .type = U_NETWORK_TYPE_BLE,
-        .role = U_BLE_CFG_ROLE_CENTRAL,
-        .spsServer = false,
-    };
-
     // Remove the line below if you want the log printouts from ubxlib
     //uPortLogOff();
     // Initiate ubxlib
@@ -55,25 +79,32 @@ void main()
     uDeviceInit();
     // And the U-blox module
     int32_t errorCode;
-    uDeviceGetDefaults(U_DEVICE_TYPE_SHORT_RANGE, &gDeviceCfg);
+    uDeviceHandle_t deviceHandle;
+
+    uDeviceGetDefaults(gDeviceType, &gDeviceCfg);
+    gDeviceCfg.deviceCfg.cfgSho.moduleType = U_SHORT_RANGE_MODULE_TYPE_NINA_W15;
     printf("\nInitiating the module...\n");
-    errorCode = uDeviceOpen(&gDeviceCfg, &gDeviceHandle);
+    errorCode = uDeviceOpen(&gDeviceCfg, &deviceHandle);
+
     if (errorCode == 0) {
-        printf("Starting BLE...\n");
-        errorCode = uNetworkInterfaceUp(gDeviceHandle, networkCfg.type, &networkCfg);
+        printf("Bringing up the ble network...\n");
+        errorCode = uNetworkInterfaceUp(deviceHandle, gNetworkCfg.type, &gNetworkCfg);
+
         if (errorCode == 0) {
-            errorCode = uBleGapScan(gDeviceHandle,
-                                    U_BLE_GAP_SCAN_DISCOVER_ALL_ONCE,
-                                    true, 10000,
-                                    scanResponseCb);
-            if (errorCode != 0) {
-                printf("* Failed to start scanning: %d", errorCode);
+            uBleSpsSetCallbackConnectionStatus(deviceHandle, connectionCallback, &deviceHandle);
+            uBleSpsSetDataAvailableCallback(deviceHandle, dataAvailableCallback, &deviceHandle);
+
+            printf("\n== Start a SPS client e.g. in a phone ==\n\n");
+            printf("Waiting for connections...\n");
+            while (1) {
+                uPortTaskBlock(1000);
             }
         } else {
             printf("* Failed to bring up the network: %d\n", errorCode);
         }
+
+        uDeviceClose(deviceHandle, true);
     } else {
         printf("* Failed to initiate the module: %d\n", errorCode);
     }
-    printf("Done\n");
 }
