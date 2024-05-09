5a925a6e68c2c1b1f47af5cbe3998d5334dbf73f
Anderson Felippe <adfelippe@gmail.com>
2024-05-10 00:14:36 +0200

    Improve transmission protocol

--- doom-port/ubx_doom_port.c
+++ doom-port/ubx_doom_port.c
@@ -12,14 +12,12 @@
 #define DOOM_FRAME_SIZE     (DOOMGENERIC_RESX * DOOMGENERIC_RESY * 4)
 #define SINGLE_PACKET_SIZE  242
 
-const char gStartOfFrame[] = {0xCA, 0xFE, 0xBA, 0xBE};
-const char gEndOfFrame[] = {0xDE, 0xAD, 0xBE, 0xEF};
+char gStartOfFrame[] = {0xCA, 0xFE, 0xBA, 0xBE, 0, 0, 0, 0};
 
 static uDeviceType_t gDeviceType = U_DEVICE_TYPE_SHORT_RANGE;
 static const uNetworkCfgBle_t gNetworkCfg = {
     .type = U_NETWORK_TYPE_BLE,
-    .role = U_BLE_CFG_ROLE_PERIPHERAL,
-    .spsServer = true
+    .role = U_BLE_CFG_ROLE_PERIPHERAL
 };
 static uDeviceCfg_t gDeviceCfg;
 static volatile bool gIsConnected = false;
@@ -33,45 +31,28 @@ static void connectionCallback(int32_t connHandle, char *address, int32_t status
                                int32_t channel, int32_t mtu, void *pParameters)
 {
     if (status == (int32_t)U_BLE_SPS_CONNECTED) {
-        printf("Connected to: %s, channel: %d, mtu: %d\n", address, channel, mtu);
-        gIsConnected = true;
+        uBleSpsSetSendTimeout(gDeviceHandle, gSpsChannel, 1000);
         gSpsChannel = channel;
         gMtuSize = mtu;
+        gIsConnected = true;
+        printf("Connected to: %s, channel: %d, mtu: %d\n", address, channel, mtu);        
     } else if (status == (int32_t)U_BLE_SPS_DISCONNECTED) {
         if (connHandle != U_BLE_SPS_INVALID_HANDLE) {
-            printf("Disconnected\n");
             gIsConnected = false;
+            printf("Disconnected\n");
         } else {
-            printf("* Connection attempt failed\n");
+            printf("Connection attempt failed\n");
         }
     }
-}
-
-static size_t covertFrameToPng(uint8_t *pPngArray) {
-    size_t pngSize;
-    uint8_t *pScreenBuffer = (uint8_t *)DG_ScreenBuffer;
-    uint8_t pImageBuffer[DOOM_FRAME_SIZE];
-
-    for (uint32_t i = 0; i < DOOM_FRAME_SIZE; i += 4) {
-        pImageBuffer[i] = pScreenBuffer[i + 2];
-        pImageBuffer[i + 1] = pScreenBuffer[i + 1];
-        pImageBuffer[i + 2] = pScreenBuffer[i];
-        pImageBuffer[i + 3] = 0xFF;
-    }
-
-    uint32_t error = lodepng_encode32(&pPngArray, &pngSize, pImageBuffer, DOOMGENERIC_RESX, DOOMGENERIC_RESY);
-    if (error) {
-        printf("lodepng error %u: %s\n", error, lodepng_error_text(error));
-        pngSize = 0;
-    }
-
-    return pngSize;
 }
 
 static void sendBle(const uint8_t *data, uint32_t size)
 {
     if (gIsConnected) {
-        uBleSpsSend(gDeviceHandle, gSpsChannel, data, size);
+        int32_t bytesSent = 0;
+        while (bytesSent < size) {
+            bytesSent = uBleSpsSend(gDeviceHandle, gSpsChannel, &data[bytesSent], size - bytesSent);
+        }
     }
 }
 
@@ -130,29 +111,35 @@ void DG_DrawFrame(void)
             printf("lodepng error %u: %s\n", error, lodepng_error_text(error));
             pngSize = 0;
         }
-        printf("pngSize = %lu\n", pngSize);
+        printf("pngSize = %zu\n", pngSize);
 
         if (pngSize) {
-            uint32_t packetsToSend = pngSize / SINGLE_PACKET_SIZE;
-            uint32_t remainder = pngSize % SINGLE_PACKET_SIZE;
+            uint32_t packetsToSend = pngSize / gMtuSize;
+            uint32_t remainder = pngSize % gMtuSize;
             uint32_t offset = 0;
+            uint8_t *pPngSizeArray = (uint8_t *)&pngSize;
 
-            // printf("packetsToSend = %u\n", packetsToSend);
-            // printf("remainder = %u\n", remainder);
+            //printf("packetsToSend = %u\n", packetsToSend);
+            //printf("remainder = %u\n", remainder);
 
+            // Copy PNG size in bytes to start of frame - remote will expect that number of bytes
+            gStartOfFrame[4] = pPngSizeArray[3];
+            gStartOfFrame[5] = pPngSizeArray[2];
+            gStartOfFrame[6] = pPngSizeArray[1];
+            gStartOfFrame[7] = pPngSizeArray[0];
             sendBle(gStartOfFrame, sizeof(gStartOfFrame));
+            //DG_SleepMs(10);
 
             for (uint32_t i = 0; i < packetsToSend; ++i) {
-                sendBle(&pPngArray[offset], SINGLE_PACKET_SIZE);
-                offset += SINGLE_PACKET_SIZE;
-                DG_SleepMs(10);
+                sendBle(&pPngArray[offset], gMtuSize);
+                offset += gMtuSize;
+                //DG_SleepMs(10);
             }
 
             if (remainder) {
                 sendBle(&pPngArray[offset], remainder);
             }
 
-            sendBle(gEndOfFrame, sizeof(gEndOfFrame));
             free(pPngArray);
         }
     } else {
